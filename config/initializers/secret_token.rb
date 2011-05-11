bc054fa274228b3b63983b81eb7b026a2ba35958
sralston <sralston@gmail.com>
2011-05-11 14:35:57 -0700

    just started, updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '910fbadbddd452ba6bdacc89e35981b053dd7e24dd90d81bad8a4bd1e57a5546f1cc7a5a50b3126e8a21542d6449e49c9f5f6a031abb90b65a1a2157763c970a'
