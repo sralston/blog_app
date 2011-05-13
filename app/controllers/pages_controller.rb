b73059c732200931793123abf902ab9a93303797
sralston <sralston@gmail.com>
2011-05-13 00:44:11 -0700

    starting new branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,27 @@
 class PagesController < ApplicationController
   def home
   	@title = "| Home Page & Blog"
+  	@nav_current = "home"
   end
 
   def about
   	@title = "| About"
+  	@nav_current = "about"
   end
 
   def blogroll
   	@title = "| Blogroll"
+  	@nav_current = "blogroll"
   end
 
-  def articles
-  	@title = "| Articles"
+  def contact
+  	@title = "| Contact"
+  	@nav_current = "contact"
+  end
+  
+  def donate
+  	@title = "| Donate"
+  	@nav_current = "donate"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 BlogApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/about"
-
   get "pages/blogroll"
-
-  get "pages/articles"
+  get "pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
